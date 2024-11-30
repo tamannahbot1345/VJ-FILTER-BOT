3f1177c649f201aa7bd14e204027d29da780b4d7
tamannahbot1345 <tamannahbotv01@gmail.com>
2024-11-30 09:33:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -66,7 +66,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 # MongoDB information
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://theatrebot007:theatrepun@cluster0.erdf64v.mongodb.net/?retryWrites=true&w=majority")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://theatrebot007:NewTheatreDb@cluster0.nf3m2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "theatrebot007")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
